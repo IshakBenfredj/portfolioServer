ab34b9c736627faf86dcc36b624c83d96b529dd3
Ishak Benfredj <ishakbanfradje@gmail.com>
2026-09-21 23:36:22 +0100

    v3

--- index.js
+++ index.js
@@ -4,46 +4,55 @@ const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // Import Routes
-const skillsRoutes = require('./routes/skills-routes')
-const portfolioRoutes = require('./routes/portfolio-routes');
-const servicesRoutes = require('./routes/services-routes');
-const testimonialsRoutes = require('./routes/testimonials-routes');
-const messagesRoutes = require('./routes/messages-routes');
-const commentsRoutes = require('./routes/comments-routes');
-const lessonsRoutes = require('./routes/lessons-routes');
-const productsRoutes = require('./routes/products-routes');
-const ordersRoutes = require('./routes/orders-routes');
-const adminRoutes = require('./routes/admin-routes');
+const skillsRoutes = require("./routes/skills-routes");
+const portfolioRoutes = require("./routes/portfolio-routes");
+const servicesRoutes = require("./routes/services-routes");
+const testimonialsRoutes = require("./routes/testimonials-routes");
+const messagesRoutes = require("./routes/messages-routes");
+const commentsRoutes = require("./routes/comments-routes");
+const lessonsRoutes = require("./routes/lessons-routes");
+const productsRoutes = require("./routes/products-routes");
+const ordersRoutes = require("./routes/orders-routes");
+const adminRoutes = require("./routes/admin-routes");
+const job = require("./cron");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json({ limit: '100mb' }));
+job.start();
+
+app.use(express.json({ limit: "100mb" }));
 
 app.use(cors());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use(morgan('common'));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true, parameterLimit: 50000 }));
-app.use(bodyParser.text({ limit: '200mb' }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "100mb",
+    extended: true,
+    parameterLimit: 50000,
+  }),
+);
+app.use(bodyParser.text({ limit: "200mb" }));
 
 // Routes
-app.use('/admin', adminRoutes)
-app.use('/skills', skillsRoutes)
-app.use('/portfolio', portfolioRoutes)
-app.use('/services', servicesRoutes)
-app.use('/testimonials', testimonialsRoutes)
-app.use('/messages', messagesRoutes)
-app.use('/comments', commentsRoutes)
-app.use('/lessons', lessonsRoutes)
-app.use('/products', productsRoutes)
-app.use('/orders', ordersRoutes)
+app.use("/admin", adminRoutes);
+app.use("/skills", skillsRoutes);
+app.use("/portfolio", portfolioRoutes);
+app.use("/services", servicesRoutes);
+app.use("/testimonials", testimonialsRoutes);
+app.use("/messages", messagesRoutes);
+app.use("/comments", commentsRoutes);
+app.use("/lessons", lessonsRoutes);
+app.use("/products", productsRoutes);
+app.use("/orders", ordersRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -55,6 +64,5 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
 
 app.timeout = 600000;
